7e73c3f0a47612f1cc874b31799c812e5c2cba33
Sitinurfaiza <112322032+Sitinurfaiza@users.noreply.github.com>
2023-10-30 15:47:57 +0700

    Update connect.php

--- Proses/connect.php
+++ Proses/connect.php
@@ -1,6 +1,6 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","db_decafe");
-    if(!$conn){
-        echo "gagal koneksi";
-    }
+    $conn = mysqli_connect("u837952942_siti","root","3UyQ3J3fBe[","u837952942_siti");
+    //if(!$conn){
+    //    echo "gagal koneksi";
+    //}
 ?>
